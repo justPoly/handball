da45e61adb03ab29cb245af8a830b33fec5c33e7
PEMAWA <petermaina546@gmail.com>
2021-11-16 15:22:52 +0300

    Added follow ball script

---
+++ Assets/Scripts/FollowBall.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowBall : MonoBehaviour
+{
+    
+    
+   public Transform ball;
+    private Vector3 offset;
+
+   
+    void Start()
+    {
+        offset = transform.position - ball.position;
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 newPosition = new Vector3(transform.position.x, transform.position.y, offset.z + ball.position.z);
+        transform.position = newPosition;
+
+    }
+}
+
