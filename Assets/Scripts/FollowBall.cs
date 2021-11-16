672678f762c5ea672ff3f3957e2d688bf4266b13
PEMAWA <petermaina546@gmail.com>
2021-11-16 16:02:38 +0300

    Added FollowBall script

--- Assets/Scripts/FollowBall.cs
+++ Assets/Scripts/FollowBall.cs
@@ -20,6 +20,7 @@ public class FollowBall : MonoBehaviour
     void LateUpdate()
     {
         Vector3 newPosition = new Vector3(transform.position.x, transform.position.y, offset.z + ball.position.z);
+
         transform.position = newPosition;
 
     }
