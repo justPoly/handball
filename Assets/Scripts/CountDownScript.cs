37401bcb5e58dcef611a8b99531733d1619ec8d1
Omoerah Felix <felixomoerah@gmail.com>
2021-11-18 15:36:41 +0100

    Felix update
    First commit by Felix

---
+++ Assets/Scripts/CountDownScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDownScript : MonoBehaviour
+{
+    public int CountdownTime;
+    public Text CountdownDisplay;
+
+    private void Start()
+    {
+        StartCoroutine(CountdownToStart());
+    }
+
+    IEnumerator CountdownToStart()
+    {
+        while(CountdownTime > 0)
+        {
+            CountdownDisplay.text = CountdownTime.ToString();
+            yield return new WaitForSeconds(1f);
+
+            CountdownTime--;
+        }
+        CountdownDisplay.text = "GO!";
+
+
+        yield return new WaitForSeconds(1f);
+
+        CountdownDisplay.gameObject.SetActive(false);
+
+    }
+}
