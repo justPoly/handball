514b59d4094d1958f52d12d12c1dd9472ea88408
Teresiah6 <wangariteresiah6@gmail.com>
2021-11-18 12:02:05 +0300

    Ball controller

---
+++ Assets/Scripts/Ball1.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+   
+    public Vector3 ballattachment;
+    private Rigidbody ballRb;
+    public Transform player;
+
+    public float ballThrowingForce = 5f;
+    public float ballDistance = 2f;
+    private bool holdingBall = true;
+    
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        ballRb = GetComponent<Rigidbody>();
+        transform.position = ballattachment;
+        ballRb.useGravity = false;
+    }
+  // Update is called once per frame
+    void Update()
+    {
+       /* if (holdingBall)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+              
+
+                ballRb.AddForce(transform.forward * ballThrowingForce);
+
+            }
+          
+        }*/
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            transform.position = ballattachment;
+            collision.transform.parent = ballRb.transform;
+        }
+    }
+}
+
---
+++ Assets/Scripts/BallBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBehavior : MonoBehaviour
+{
+    public Vector3 ballattachment;
+    private Rigidbody ballRb;
+    public Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ballRb = GetComponent<Rigidbody>();
+        transform.position = ballattachment;
+        ballRb.useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            transform.position = ballattachment;
+            collision.transform.parent = ballRb.transform;
+        }
+    }
+}
