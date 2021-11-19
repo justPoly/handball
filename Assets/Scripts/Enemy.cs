f58fad9e1adf02ae708073cc6bb48beaaa4d83c8
kenjoel <jrjoemuigai@gmail.com>
2021-11-19 13:19:03 -0800

    Enemy as Nav Mesh agent

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    private Ball ball;
+    public float chasingDistance = 12f;
+    public float chasingInterval = 2f;
+    public float chasingTimer;
+
+    public float speed = 8f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ball = GameObject.Find("Ball").GetComponent<Ball>();
+        Debug.Log(ball);
+        agent = GetComponent<NavMeshAgent>(); 
+
+        agent.SetDestination(ball.transform.position);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        chasingTimer -= Time.deltaTime;
+
+        if (chasingTimer <= 0)
+        {
+            chasingTimer = chasingInterval;
+            agent.SetDestination(ball.transform.position);
+
+        }
+    }
+
+}
